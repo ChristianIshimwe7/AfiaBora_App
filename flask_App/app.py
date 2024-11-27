ee711fabdad921157a6d5900e2c9472c337931b0
ChristianIshimwe7 <c.ishimwe7@alustudent.com>
2024-11-27 17:23:47 +0200

    msql and update

--- flask_App/app.py
+++ flask_App/app.py
@@ -4,10 +4,10 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 # MySQL configuration
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Ishichristian@1'
-app.config['MYSQL_DB'] = 'medical_db'
+app.config['MYSQL_HOST'] = '*********'
+app.config['MYSQL_USER'] = '******'
+app.config['MYSQL_PASSWORD'] = '*********'
+app.config['MYSQL_DB'] = '*************'
 
 mysql = MySQL(app)
 
